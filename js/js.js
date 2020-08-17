71349a1760686623f50667f122f1d4c8cab17084
Olga-smart <33065109+Olga-smart@users.noreply.github.com>
2020-08-17 19:35:51 +0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,6 +1,9 @@
 const search = document.querySelector('.search-in-nav');
-const body = document.querySelector('body');
+const body = document.body;
+const footer = document.querySelector('.page-footer');
+let footerHeight = footer.offsetHeight;
 
+// Make Search in Nav to Work
 search.addEventListener('click', function(e) {
 	e.stopPropagation();
 	this.classList.add('active');
@@ -9,4 +12,18 @@ search.addEventListener('click', function(e) {
 body.addEventListener('click', function() {
 	search.classList.remove('active');
 });
+// End Make Search in Nav to Work
+
+
+// Fix footer on the bottom of the page
+document.addEventListener('DOMContentLoaded', function() {
+	footerHeight = footer.offsetHeight;
+	body.style.paddingBottom = footerHeight + 'px';
+});
+
+window.addEventListener('resize', function() {
+	footerHeight = footer.offsetHeight;
+	body.style.paddingBottom = footerHeight + 'px';
+});
+// End Fix footer on the bottom of the page
 
